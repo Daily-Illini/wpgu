78bab6a59f27bbbde6973737834f7932e8d61d7d
Matthew Novelli <mattnovelli@gmail.com>
2022-10-03 21:46:10 -0500

    minutes ago working

--- javascript/populate_recents.js
+++ javascript/populate_recents.js
@@ -1,10 +1,18 @@
 const requestURL = 'https://streamdb8web.securenetsystems.net/player_status_update/WPGUFM_history.txt';
 
-const section = document.querySelector('#recently-played-section');
+const recently_played_section = document.querySelector('#recently-played-section');
 
+function calculateTimeSince(then) {
+
+    then.setHours(then.getHours() - 5)
+
+    var now = new Date(Date.now())
+
+    var diff = then - now
+    return (Math.floor(Math.abs((then - now)/60000)) + " minutes ago")
+}
 
 function newSong(song) {
-
     const song_node = document.createElement('div')
     song_node.className = "recently-played-node";
 
@@ -18,15 +26,13 @@ function newSong(song) {
     song_title.id = "recently-played-title"
     song_title.textContent = song.title
 
-
     const song_artist = document.createElement("div")
     song_artist.id = "recently-played-artist"
     song_artist.textContent = song.artist
 
     const song_time = document.createElement("div")
     song_time.id = "recently-played-time"
-    song_time.textContent = song.programStartTS
-
+    song_time.textContent = calculateTimeSince(new Date(song.programStartTS))  
 
     if (song.cover.length != 0) {
         song_image.src = song.cover;
@@ -34,32 +40,32 @@ function newSong(song) {
         song_image.src = "./assets/placeholder.svg"
     }
 
-
     text_container.appendChild(song_title)
     text_container.appendChild(song_artist)
 
-
-    section.appendChild(song_node).appendChild(song_image)
-    section.appendChild(song_node).appendChild(text_container).appendChild(song_time)
-    section.appendChild(song_node).appendChild(song_time)
-
-
-
+    recently_played_section.appendChild(song_node).appendChild(song_image)
+    recently_played_section.appendChild(song_node).appendChild(text_container).appendChild(song_time)
+    recently_played_section.appendChild(song_node).appendChild(song_time)
 }
 
-function populate(obj) {
-    for(let i = 1; i < obj.length; i++) {
-        if (obj[i].duration == 0) {
+function populate(songs) {
+    if (songs[0].cover.length != 0) {
+        document.getElementById("dynamicImg").src = songs[0].cover
+    } else {
+        document.getElementById("dynamicImg").src = "./assets/placeholder.svg"
+    }
+
+    document.getElementById("dynamicTitle").innerHTML = songs[0].title
+    document.getElementById("dynamicArtist").innerHTML = songs[0].artist
+    
+    for(let i = 1; i < songs.length; i++) {
+        if (songs[i].duration == 0 || String(songs[i].title).match(/COM[0-9]{4}/)) {
             continue
         } else {
-            newSong(obj[i])
-            console.log(obj[i].title);
+            newSong(songs[i])
         }
     }
 }
-
-// document.getElementById("dynamicTitle").innerHTML = titleNode[0].childNodes[0].nodeValue;
-
 
 fetch(requestURL)
     .then( res => { return res.json(); } )
